e46a0894df9af9ee3de69c3ddce15662c2c0ca85
Raymond <Raymondwong1998@outlook.com>
2026-05-03 20:56:49 +0100

    - about us implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import './App.css'
 import MainLayout from './layouts/MainLayout'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import AboutUs from './components/AboutUs'
 
 export default function App() {
   return (
     <MainLayout>
       <Navbar />
       <Hero />
+      <AboutUs />
     </MainLayout>
+    
   )
 }
